720ca58f0b76d10ae98a8cacf0473ff36d28b0ad
Tanai thassanapinyo <charlot@tanais-macbook-pro.local>
2018-11-09 16:06:18 +0700

    Initial Commit

---
+++ 13600178_miniproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  13600178_miniproject
+//
+//  Created by Tanai thassanapinyo on 9/11/2561 BE.
+//  Copyright © 2561 Tanai Thassanapinyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
